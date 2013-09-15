7aefa5455c695e46f84d30332b0ada686dc3d3ec
Dominik Schöni <ds@clubzone.ch>
2013-09-15 21:02:44 +0200

    Wow! I did this last night!
    Please check out these changes I made.

---
+++ RectangularShape.java
@@ -0,0 +1,29 @@
+
+public class RectangularShape extends Shape {
+	
+	private double length;
+	private double width;
+
+	public RectangularShape(double length, double width, double x_center, double y_center) {
+		super(x_center, y_center);
+		this.length = length;
+		this.width = width;
+	}
+
+	public double getDiagonalLength()
+	{
+		double fullWidth = (width * width);
+		double fullLength = (length * length);
+		double bothAdded = fullWidth + fullLength;
+		
+		double result = Math.sqrt(bothAdded);
+		
+		return result;
+	}
+	
+	public double computeArea() {
+		return length * width;
+	}
+	
+	
+}
